63e4226f00208dcb40272d453a466cc28a873ed9
Mihai Ghenciu <mghenciu@endava.com>
2018-06-24 00:22:04 +0300

    Detail page routing & minor fixes

--- server/controllers/views.js
+++ server/controllers/views.js
@@ -34,4 +34,9 @@ module.exports = (app) => {
     const authenticated = (req.user) ? true : false;
     res.render('contact.html', { user_authenticated: authenticated });
   });
+
+  app.get('/detailpage', (req, res) => {
+    const authenticated = (req.user) ? true : false;
+    res.render('detailpage.html', { user_authenticated: authenticated });
+  });
 };
