64156cd1aa5bd8afe53f6be0f24a46f0bb57a950
Nick Spirit <nick@hdigital.io>
2018-06-24 13:15:39 +0300

    updated shit

--- server/controllers/donation.js
+++ server/controllers/donation.js
@@ -6,28 +6,35 @@ module.exports = (app) => {
   app.post('/donate', (req, res) => {
     req.assert('companyEmail', 'Email is not valid').isEmail();
     req.assert('walletAddress', 'Provide a valid wallet address').notEmpty();
-
+    console.log(req.body);
+    console.log(req.body.now);
+    console.log(req.body.later);
     const errors = req.validationErrors();
     console.log(errors);
+
     if (errors) {
       req.flash('errors', errors);
       return res.redirect('/fha');
     }
 
-    return res.redirect('/donate-history');
+    if (req.body.now) {
+      req.flash('warning', 'now')
+    }
+
+    return res.redirect('/fha');
   });
 
-  app.get('/donate-history', (req, res) => {
-    req.session.donationHistory = ['hello']
-    return res.redirect(url.format({
-      pathname: '/fha',
-      query: {
-        a: ['aw', 'sdf'],
-        b: 2,
-        valid: 'your string here'
-      }
-    }));
+  // app.get('/donate-history', (req, res) => {
+  //   req.session.donationHistory = ['hello']
+  //   return res.redirect(url.format({
+  //     pathname: '/fha',
+  //     query: {
+  //       a: ['aw', 'sdf'],
+  //       b: 2,
+  //       valid: 'your string here'
+  //     }
+  //   }));
 
-  });
+  // });
 
 }
